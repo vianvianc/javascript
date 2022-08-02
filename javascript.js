7e80d7f077dba1102430dffa6782b95e7cc89a88
Vian Charbonneau <102205118+vianvianc@users.noreply.github.com>
2022-08-02 14:36:31 -0600

    added js events

--- javascript.js
+++ javascript.js
@@ -43,32 +43,30 @@
 // }
 // console.log(result); // → 1024
 
-for (let hash = "#"; hash.length <= 7; hash += hash) {
+for (let hash = "#"; hash.length <= 7; hash += "#") {
     console.log(hash);
 }
 
 
 
+let number = 0;
+while (number <= 12) {
+    console.log(number);
+    number = number + 2;
+}
+// → 0
+// → 2
+// ...   etcetera
+ //these two are identical
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for (let number = 0; number <= 12; number = number + 2) {
+    console.log(number);
+}
+// → 0
+// → 2
+// ...   etcetera
 
 
 //
@@ -83,3 +81,19 @@ for (let hash = "#"; hash.length <= 7; hash += hash) {
 // // (This is actually an interview question that has been claimed to weed out a significant
 // //percentage of programmer candidates. So if you solved it, your labor market value just went up.)
 // for (let number = 1; number.length < 100; number += 1) console.log(number);
+
+
+//
+// Looping a triangle
+// You can start with a program that prints out the numbers 1 to 7, which you can derive by making a few modifications to the even number printing example given earlier in the chapter, where the for loop was introduced.
+//     Now consider the equivalence between numbers and strings of hash char- acters. You can go from 1 to 2 by adding 1 (+= 1). You can go from "#" to "##" by adding a character (+= "#"). Thus, your solution can closely follow the number-printing program.
+//     FizzBuzz
+// Going over the numbers is clearly a looping job, and selecting what to print is a matter of conditional execution. Remember the trick of using the remainder (%) operator for checking whether a number is divisible by another number (has a remainder of zero).
+// In the first version, there are three possible outcomes for every number, so you’ll have to create an if/else if/else chain.
+//     The second version of the program has a straightforward solution and a clever one. The simple solution is to add another conditional “branch” to precisely test the given condition. For the clever solution, build up a string containing the word or words to output and print either this word or the number if there is no word, potentially by making good use of the || operator.
+// 388
+// Chessboard
+// You can build the string by starting with an empty one ("") and repeatedly adding characters. A newline character is written "\n".
+//     To work with two dimensions, you will need a loop inside of a loop. Put braces around the bodies of both loops to make it easy to see where they start and end. Try to properly indent these bodies. The order of the loops must follow the order in which we build up the string (line by line, left to right, top to bottom). So the outer loop handles the lines, and the inner loop handles the characters on a line.
+//     You’ll need two bindings to track your progress. To know whether to put a space or a hash sign at a given position, you could test whether the sum of the two counters is even (% 2).
+// Terminating a line by adding a newline character must happen after the line has been built up, so do this after the inner loop but inside the outer loop.
